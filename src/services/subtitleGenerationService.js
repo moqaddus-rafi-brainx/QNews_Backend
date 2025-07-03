f33c03e0b68f08e8c2e7064fca60191acd88f67f
moqaddus-rafi-brainx <moqaddus.rafi@brainxtech.com>
2025-07-03 13:38:05 +0500

    Changes in subtitle generation

--- src/services/subtitleGenerationService.js
+++ src/services/subtitleGenerationService.js
@@ -357,12 +357,13 @@ You are a professional subtitle file generator. Your task is to create high-qual
 Instructions:
 1. Translate the text to natural, fluent English.
 2. Break long segments into multiple short lines for subtitles.
-3. Each subtitle must be at max 15 words long and at min 12 words(if possible).
+3. Each subtitle must be at max 15 words long and at min 12 words(if possible). First find out the number of subtitles the transcript is divided into, then divide the time duration of the segment transcript among all subtitles equally.
 4. All subtitles should be within the following time range(hh:mm:ss,mmm): (${secondsToSRTTime(transcriptData[0].startTime)} - ${secondsToSRTTime(transcriptData[transcriptData.length - 1].endTime)})
 5. Ensure each subtitle line is synchronized with the corresponding transcript timestamp.
 6. IMPORTANT: ENSURE that no 2 subtitles have same or overlapping timestamp.
 7. Format the output as a valid SRT file with EXACT formatting.
-8. IMPORTANT: All of the transcripts(in chunks) MUST be included in the SRT file.You may decrease the timespan of each subtitle so that all subtitles can fit in the video timestamp.
+8. IMPORTANT: All of the transcripts(in chunks) MUST be included in the SRT file.
+9. Show the last subtitle till the end of transcript duration.
 
 All of he subtitles must be shown before the end of the video.
 
